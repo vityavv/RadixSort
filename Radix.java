9c310875159ff89b9bb5b97e6f26dd76a83848ac
Victor Veytsman <vityavv@gmail.com>
2021-01-11 14:57:36 -0500

    Add nth, length, and merge functions

---
+++ Radix.java
@@ -0,0 +1,13 @@
+public class Radix {
+	public static int nth(int n, int col) {
+		return (n / (int) Math.pow(10,col)) % 10;
+	}
+	public static int length(int n) {
+		return (int) Math.log10(n) + 1;
+	}
+	public static void merge(MyLinkedList original, MyLinkedList []buckets) {
+		for (MyLinkedList bucket : buckets) {
+			original.extend(bucket);
+		}
+	}
+}
